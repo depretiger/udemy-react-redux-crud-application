182684a3894223f577dce9cd3b03c7986366d546
depretiger <depretiger@gmail.com>
2021-12-07 17:15:08 +0900

    Fix worning export defalt function

--- src/reducers/count.js
+++ src/reducers/count.js
@@ -2,7 +2,8 @@ import { INCREMENT, DECREMENT } from "../actions"
 
 const initialState = { value: 0 }
 
-export default (state = initialState, action) =>  {
+
+const switchEvents = (state = initialState, action) =>  {
     switch (action.type) {
         case INCREMENT:
             return { value: state.value + 1 }
@@ -12,3 +13,5 @@ export default (state = initialState, action) =>  {
             return state
     };
 }
+
+export default switchEvents;
