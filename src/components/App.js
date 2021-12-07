cfe9e95f11446d60815c77d586bfc32f9c407359
depretiger <depretiger@gmail.com>
2021-12-07 17:38:47 +0900

    implement component with connect function

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,34 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
 
-const App = () => (<Counter /> )
-
-class Counter extends Component{
-  constructor(props) {
-    super(props);
-    this.state = { count: 0}
-    console.log(this.state.count);
-  }
-  
-  handlePlusButton = () => {
-    this.setState({count: this.state.count + 1});
-  }
-
-  handleMinusButton = () => {
-    this.setState({count: this.state.count - 1});
-  }
-
+import { increment, decrement} from '../actions';
+class App extends Component{
   render() {
+    const props = this.props;
+
     return (
       <React.Fragment>
-      <div>count: {this.state.count}</div>
-      <button onClick = {this.handlePlusButton}>+1</button>
-      <button onClick = {this.handleMinusButton}>-1</button>
+        <div>value: { props.value }</div>
+        <button onClick = {props.increment}>+1</button>
+        <button onClick = {props.decrement}>-1</button>
       </React.Fragment>
     );
   }
 }
 
-export default App;
+// mapStateToProps: stateの情報からこのコンポーネントに必要なものを取り出して、このコンポーネントのpropsにマッピングする関数
+// どういったオブジェクトをpropsにマッピングするかを戻り値として返す　
+const mapStateToProps = (state) => ({ value: state.count.value});
+
+// mapDispatchToProps: あるアクションが発生したときに、Reducerにタイプに応じた状態遷移を実行させるためのDispatch関数を引数に取る
+// const mapDispatchToProps = (dispatch) => ({
+//   increment: () => dispatch(increment()),
+//   decrement: () => dispatch(decrement())
+// });
+
+// 省略形
+const mapDispatchToProps = ({increment, decrement})
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+
