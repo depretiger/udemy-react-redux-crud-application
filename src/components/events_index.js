1bbabdb82d24cf8c02c7a5ca0a62311380797b6b
depretiger <depretiger@gmail.com>
2021-12-08 18:00:32 +0900

    implement events index

---
+++ src/components/events_index.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import { connect } from 'react-redux';
+import _ from 'lodash';
+
+import { readEvents } from '../actions';
+class EventsIndex extends Component{
+  componentDidMount() {
+    this.props.readEvents()
+  }
+  renderEvents() {
+    return _.map(this.props.events, event => (
+      <tr key={event.id}>
+        <td>{event.id}</td>
+        <td>{event.title}</td>
+        <td>{event.body}</td>
+      </tr>
+      ))
+  }
+
+  render() {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Body</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.renderEvents()}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+// mapStateToProps: stateの情報からこのコンポーネントに必要なものを取り出して、このコンポーネントのpropsにマッピングする関数
+// どういったオブジェクトをpropsにマッピングするかを戻り値として返す　
+const mapStateToProps = (state) => ({events: state.events});
+
+// mapDispatchToProps: あるアクションが発生したときに、Reducerにタイプに応じた状態遷移を実行させるためのDispatch関数を引数に取る
+// const mapDispatchToProps = (dispatch) => ({
+//   increment: () => dispatch(increment()),
+//   decrement: () => dispatch(decrement())
+// });
+
+// 省略形
+const mapDispatchToProps = ({ readEvents })
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(EventsIndex);
+
